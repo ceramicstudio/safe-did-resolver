037462d4ae86fb2776102e7eaef9c34056e6ade1
Sergey Ukustov <sergey@ukstv.me>
2022-04-11 13:39:10 +0300

    chore: Update deps

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,10 +1,12 @@
-import { GlobalWithFetchMock } from 'jest-fetch-mock';
+import { jest } from '@jest/globals'
+import { GlobalWithFetchMock } from 'jest-fetch-mock'
+import jestFetchMock from 'jest-fetch-mock'
 
-const customGlobal = global as unknown as GlobalWithFetchMock;
-customGlobal.fetch = require('jest-fetch-mock');
-customGlobal.fetchMock = customGlobal.fetch;
+const customGlobal = global as unknown as GlobalWithFetchMock
+customGlobal.fetch = jestFetchMock
+customGlobal.fetchMock = customGlobal.fetch
 
-jest.setMock('cross-fetch', fetch); // Use this to mock your ponyfilled fetch module
+jest.setMock('cross-fetch', fetch) // Use this to mock your ponyfilled fetch module
 
 // don't mock by default
-customGlobal.fetchMock.dontMock();
+customGlobal.fetchMock.dontMock()
